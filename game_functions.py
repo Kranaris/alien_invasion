7ab656b510044816bcc94dda3e9054569e54b6b9
NikitaKaravaev25 <nikitakaravaev@mail.ru>
2022-07-07 17:07:38 +0300

    speedup game score_not work

--- game_functions.py
+++ game_functions.py
@@ -69,14 +69,14 @@ def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_bu
 
 
 
-def update_bullets(ai_settings, screen, ship, aliens, bullets):
+def update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """Обновляет позиции пуль и удаляет старые пули"""
     bullets.update()
     # Удаление пуль
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+    check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets)
     if len(aliens) == 0:
         bullets.empty()
         create_fleet(ai_settings, screen, ship, aliens)
@@ -139,8 +139,12 @@ def change_fleet_direction(ai_settings, aliens):
     ai_settings.fleet_direction *= -1
 
 
-def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
+def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, bullets):
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    if collisions:
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings():
         # Ship
         self.ship_limit = 3
         # Bullets
-        self.bullet_width = 3
+        self.bullet_width = 500
         self.bullet_height = 15
         self.bullet_color = 100, 40, 20
         self.bullet_allowed = 3
@@ -17,6 +17,7 @@ class Settings():
         self.fleet_drop_speed = 15
         #speedup game factor
         self.speedup_scale = 1.2
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -24,8 +25,11 @@ class Settings():
         self.bullet_speed_factor = 3
         self.alien_speed_factor = 1
         self.fleet_direction = 1  # "1" - right, "-1" left
+        self.alien_points = 1
 
     def increase_speed(self):
         self.ship_speed_factor *= self.speedup_scale
         self.bullet_speed_factor *= self.speedup_scale
         self.alien_speed_factor *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
