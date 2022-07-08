3dfbedcf6afeafb4a1233acf2a0517102c20c7bd
NikitaKaravaev25 <nikitakaravaev@mail.ru>
2022-07-08 08:54:51 +0300

    work speedup and scores v2

--- game_functions.py
+++ game_functions.py
@@ -142,6 +142,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship, aliens, 
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
     sb.prep_score()
+    check_high_score(stats, sb)
     if len(aliens) == 0:
         bullets.empty()
         ai_settings.increase_speed()
@@ -165,3 +166,8 @@ def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
         if alien.rect.bottom >= screen_rect.bottom:
             ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
             break
+
+def check_high_score(stats, sb):
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
